fedd36e586d360b9e5654f923e18b62cc1c65c60
anjaleejaiswal <30697231+anjaleejaiswal@users.noreply.github.com>
2017-08-11 10:15:30 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var element =documet.getElementById('main-text');
+var element =document.getElementById('main-text');
 element.innerHTML=('new value')
 var img= document.getElementById('img');
 img.onclick=function(){
