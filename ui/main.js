031cb9ec52cd0756a790466f4fdd7f8415e63b69
anjaleejaiswal <30697231+anjaleejaiswal@users.noreply.github.com>
2017-08-12 10:22:15 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,11 @@
-console.log('Loaded!');
-var element =document.getElementById('main-text');
-element.innerHTML=('new value')
-var img= document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
+var button =button.getElementById('counter');
+var counter=0;
+button.onclick=function(){
     
+    
+    
+    
+    counter=counter+1;
+    var span= span.getElementById('count');
+    span.innerHTML=counter.toString();
 }
-img.onclick=function(){
-    
-    var interval=setInterval(moveRight,50) ;
-   
-    
-    
-} 
