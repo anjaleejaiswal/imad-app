8abafbd904b6e23ae3d1e6ce66319ecc0203977d
anjaleejaiswal <30697231+anjaleejaiswal@users.noreply.github.com>
2017-08-12 09:49:00 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,12 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('counter',function(req,res){
+   res.send(counter.toString());
+    
+});
+
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
 res.send(createTemplate (articles[articleName]));
