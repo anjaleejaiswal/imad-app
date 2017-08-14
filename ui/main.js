10d99c1756009ada55fb3eaf38694dc7f7b196b6
anjaleejaiswal <30697231+anjaleejaiswal@users.noreply.github.com>
2017-08-14 09:39:46 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,15 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
-    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    list+='<li>'+names[i]+'<li>';
 }
+var ul=document.getElemetById('namelist');
+ul.innerHTML=list;
+    
+};
     
     
     
