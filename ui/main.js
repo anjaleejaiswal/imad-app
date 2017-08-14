57f000088bdf4995ed09511fd09a99f151e97a24
anjaleejaiswal <30697231+anjaleejaiswal@users.noreply.github.com>
2017-08-14 10:44:20 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,7 @@ button.onclick=function(){
         request.send(null);
     };
     
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
@@ -32,6 +31,8 @@ submit.onclick=function()
 }
 }
         };
+        var nameInput=document.getElementById('name');
+var name=nameInput.value;
         request.open('GET','http://anjaleejaiz.imad.hasura-app.io/submit-name?name='+ name,true);
         request.send(null);
     
