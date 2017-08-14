f5a5a39900573502c36ea90f9c4ff5ca072f1381
anjaleejaiswal <30697231+anjaleejaiswal@users.noreply.github.com>
2017-08-14 09:50:43 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,12 +21,12 @@ submit.onclick=function()
 {
     var names=['name1','name2','name3','name4'];
     var list='';
-    for(var i=0;i<names.length;i++)
+    for(var i=0;i<names.length;i++){
     list+='<li>'+names[i]+'</li>';
 }
 var ul=document.getElemetById('namelist');
 ul.innerHTML=list;
-    
+}
 };
     
     
