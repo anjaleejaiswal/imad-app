e9b7764f448614594e37f1cc1a37293303b2d224
anjaleejaiswal <30697231+anjaleejaiswal@users.noreply.github.com>
2017-08-14 10:20:06 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var counter=0;
+
+app.get('/counter',function(req,res){
+    counter=counter +1;
+   res.send(counter.toString());
+    
+});
 var names=[];
 app.get('/submit-name/:name',function(req,res){
     var name=req.query.name;
@@ -99,11 +105,6 @@ app.get('/submit-name/:name',function(req,res){
     
 });
 
-app.get('/counter',function(req,res){
-    counter=counter +1;
-   res.send(counter.toString());
-    
-});
 
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
